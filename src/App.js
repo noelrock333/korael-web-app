894d4ff37ac223a9a06e2a2ea668c43763290b3d
Noel Escobedo <noel@michelada.io>
2021-02-19 22:22:11 +0000

    Firebase hosting integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
 import logo from './logo.svg';
 import './App.css';
+// import firebase from 'firebase';
+// firebase.initializeApp()
+// const database = firebase.database();
 
 function App() {
+  useEffect(() => {
+    // var starCountRef = firebase.database().ref();
+    // starCountRef.on('value', (snapshot) => {
+    //   const data = snapshot.val();
+    //   console.log(data)
+    // });
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
